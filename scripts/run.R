3a3122bbf66b27ea61d5be6377cbf5335429ccc5
zouter <wouter.saelens@gmail.com>
2018-10-23 22:00:51 +0200

    fix for new certigo

--- scripts/run.R
+++ scripts/run.R
@@ -3,7 +3,7 @@ library(readr)
 library(dplyr)
 
 # read parameters
-design <- jsonlite::fromJSON(inputs["design"])
+design <- jsonlite::fromJSON(inputs[["design"]])
 testthat::expect_true(all(names(design) %in% c("seed", "ix", names(formals(dyntoy::generate_dataset)))))
 set.seed(design$seed)
 
@@ -11,10 +11,10 @@ set.seed(design$seed)
 dataset <- purrr::invoke(dyntoy::generate_dataset, design[intersect(names(design), names(formals(dyntoy::generate_dataset)))])
 
 # write dataset
-write.csv(dataset$expression, outputs["expression"])
+write.csv(dataset$expression, outputs[["expression"]])
 
 # generate metadata
 metadata <- list(
   provides_expression = TRUE
 )
-yaml::write_yaml(metadata, outputs["meta"])
+yaml::write_yaml(metadata, outputs[["meta"]])
