9c2fa2d7b5813dec02fdf978c07cc71c83c7981e
Wouter Saelens <wouter.saelens@gmail.com>
2018-10-26 16:57:15 +0200

    tiny update

--- test.R
+++ test.R
@@ -2,7 +2,7 @@ library(certigo)
 
 datasets <- load_call(
   "workflow.R",
-  derived_file_directory = "datasets"
+  derived_file_directory = "results/datasets"
 )
 datasets$design <- datasets$design[1, ]
 datasets$start_and_wait()
